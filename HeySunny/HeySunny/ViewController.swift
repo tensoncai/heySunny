fed74a5024f1301dc59974693efcd92e9221a0ea
Darius <32181971+dariusngo@users.noreply.github.com>
2020-08-23 23:53:02 -0500

    Fix build errors

--- HeySunny/HeySunny/ViewController.swift
+++ HeySunny/HeySunny/ViewController.swift
@@ -9,21 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let cardView = LessonCardView(using: LessonCardViewModel(title: "Retirement Planning", level: "Level 0/10", playButtonImageString: "", lessonImageString: ""))
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        view.backgroundColor = UIColor(red: 0.973, green: 0.969, blue: 0.984, alpha: 1)
-        self.view.addSubview(cardView)
-        cardView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            cardView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            cardView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            cardView.widthAnchor.constraint(equalToConstant: 320),
-            cardView.heightAnchor.constraint(equalToConstant: 180),
-        ])
     }
 }
-
