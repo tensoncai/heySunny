5205da7f47e8e370ca046534ed271e8da23fd1dc
Tenson Cai <tensoncai@gmail.com>
2020-08-26 13:37:51 -0500

    TransferVC pretty much done

--- HeySunny/HeySunny/TransferVC.swift
+++ HeySunny/HeySunny/TransferVC.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class TransferVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class TransferVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    var stackView = UIStackView()
     
     let fromPicker = UIPickerView()
     let toPicker = UIPickerView()
@@ -17,11 +19,23 @@ class TransferVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     
     let fromBank: UITextField = {
         let textField = UITextField()
-        textField.text = "From Bank"
+        textField.text = " Choose"
         textField.font = UIFont.systemFont(ofSize: 24)
         textField.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1)
         textField.layer.cornerRadius = 5.0
         
+        textField.translatesAutoresizingMaskIntoConstraints = false
+
+        return textField
+    }()
+    
+    let fromText: UITextField = {
+        let textField = UITextField()
+        textField.text = "From:"
+        textField.font = UIFont.systemFont(ofSize: 24)
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5.0
+        textField.isUserInteractionEnabled = false
         textField.translatesAutoresizingMaskIntoConstraints = false
 
         return textField
@@ -29,7 +43,7 @@ class TransferVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
     
     let toBank: UITextField = {
         let textField = UITextField()
-        textField.text = "To Bank"
+        textField.text = " Choose"
         textField.font = UIFont.systemFont(ofSize: 24)
         textField.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1)
         textField.layer.cornerRadius = 5.0
@@ -39,32 +53,106 @@ class TransferVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource
         return textField
     }()
     
+    let toText: UITextField = {
+        let textField = UITextField()
+        textField.text = "To:"
+        textField.font = UIFont.systemFont(ofSize: 24)
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5.0
+        textField.isUserInteractionEnabled = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+
+        return textField
+    }()
+    
+    let amount: UITextField = {
+        let textField = UITextField()
+        textField.text = "Enter an amount"
+        textField.font = UIFont.systemFont(ofSize: 24)
+        textField.backgroundColor = UIColor(red: 237/255, green: 237/255, blue: 237/255, alpha: 1)
+        textField.layer.cornerRadius = 5.0
+        textField.keyboardType = .decimalPad
+        textField.translatesAutoresizingMaskIntoConstraints = false
+
+        return textField
+    }()
+    
+    let amountText: UITextField = {
+        let textField = UITextField()
+        textField.text = "Amount:"
+        textField.font = UIFont.systemFont(ofSize: 24)
+        textField.backgroundColor = UIColor.clear
+        textField.layer.cornerRadius = 5.0
+        textField.isUserInteractionEnabled = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+
+        return textField
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupStackView()
+        
         fromPicker.delegate = self
         toPicker.delegate = self
-        
-        self.view.addSubview(fromBank)
-        self.view.addSubview(toBank)
-        setConstraints()
-        
         fromBank.inputView = fromPicker
         toBank.inputView = toPicker
+        amount.delegate = self
+        
+        self.addDoneButtonOnKeyboard()
+    }
+    
+    private func setupStackView() {
+        stackView = UIStackView(arrangedSubviews: [fromText, fromBank, toText, toBank, amountText, amount])
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(stackView)
+        setConstraints()
     }
     
     private func setConstraints() {
-        fromBank.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
-        fromBank.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        fromBank.widthAnchor.constraint(equalToConstant: 320).isActive = true
         fromBank.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
-        toBank.topAnchor.constraint(equalTo: fromBank.bottomAnchor, constant: 40).isActive = true
-        toBank.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        toBank.widthAnchor.constraint(equalToConstant: 320).isActive = true
         toBank.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        amount.heightAnchor.constraint(equalToConstant: 40).isActive = true
         
+        stackView.widthAnchor.constraint(equalToConstant: 320).isActive = true
+        stackView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
+        stackView.setCustomSpacing(40.0, after: fromBank)
+        stackView.setCustomSpacing(40.0, after: toBank)
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        textField.text = ""
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if (textField == amount && textField.text == "") {
+            textField.text = "Enter an amount"
+        }
+    }
+    
+    func addDoneButtonOnKeyboard(){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        amount.inputAccessoryView = doneToolbar
+    }
+
+    @objc func doneButtonAction(){
+        amount.resignFirstResponder()
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
